d5f6cbe0aea285d6b38011697eb05ee67d049657
AmirHossein137 <122604637+AmirHossein137@users.noreply.github.com>
2024-07-12 22:28:32 +0330

    handle login with github

--- admin/pages/api/auth/[...nextauth].js
+++ admin/pages/api/auth/[...nextauth].js
@@ -1,11 +1,13 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
-export default NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-  ],
-});
+const nextAuthOptions = {
+    providers: [
+        GitHubProvider({
+          clientId: process.env.GITHUB_ID,
+          clientSecret: process.env.GITHUB_SECRET
+        })
+      ]
+}
+
+export default NextAuth(nextAuthOptions)
--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,21 +1,31 @@
-import { useSession, signIn, signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
+import { signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-cyan-900 w-full h-screen flex items-center justify-center">
-        <div className="text-center w-full">
-          <Button className="bg-white p-5 text-slate-950 hover:bg-gray-200 transition duration-300" onClick={() => signIn('google')}>
-            Login With Google
-          </Button>
-        </div>
-      </div>
-    );
-  }
+  const signInHandler = () => {
+    signIn("github");
+  };
+
+  const signoutHandler = () => {
+    signOut();
+  };
 
   return (
-    <div>Logged in { session.user.email }</div>
-  )
+    <div className="bg-cyan-900 w-full h-screen flex items-center justify-center">
+      <div className="text-center flex items-center justify-center gap-3 w-full">
+        <Button
+          className="bg-white p-5 text-slate-950 hover:bg-gray-200 transition duration-300"
+          onClick={signInHandler}
+        >
+          Login With GutHub
+        </Button>
+        <Button
+          className="bg-white p-5 text-slate-950 hover:bg-gray-200 transition duration-300"
+          onClick={signoutHandler}
+        >
+          SignOut
+        </Button>
+      </div>
+    </div>
+  );
 }
