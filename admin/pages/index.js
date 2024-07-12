5cc2d9e9f4000e895c12b62b0d2a2b308ad488ab
AmirHossein137 <122604637+AmirHossein137@users.noreply.github.com>
2024-07-12 21:05:44 +0330

    signin

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,8 +1,21 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-cyan-900 w-full h-screen flex items-center justify-center">
+        <div className="text-center w-full">
+          <Button className="bg-white p-5 text-slate-950 hover:bg-gray-200 transition duration-300" onClick={() => signIn('google')}>
+            Login With Google
+          </Button>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <>
-      Amir
-    </>
+    <div>Logged in { session.user.email }</div>
   )
 }
