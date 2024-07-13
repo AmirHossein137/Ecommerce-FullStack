9df97a52ab00cebdd2cdcb56de6a78e89eed56b7
AmirHossein137 <122604637+AmirHossein137@users.noreply.github.com>
2024-07-13 13:07:53 +0330

    update

--- admin/pages/api/auth/[...nextauth].js
+++ admin/pages/api/auth/[...nextauth].js
@@ -1,13 +1,18 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/db";
 
-const nextAuthOptions = {
-    providers: [
-        GitHubProvider({
-          clientId: process.env.GITHUB_ID,
-          clientSecret: process.env.GITHUB_SECRET
-        })
-      ]
-}
+const authOptions = {
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+};
 
-export default NextAuth(nextAuthOptions)
+export default NextAuth(authOptions);
+
+
